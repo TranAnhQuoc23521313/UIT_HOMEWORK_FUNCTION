b26ad9438a7350eda498780b1a84ed3549e2f826
TranAnhQuoc23521313 <146335703+TranAnhQuoc23521313@users.noreply.github.com>
2023-10-01 14:17:30 +0700

    Update 11.cpp

--- UIT_23520335_Function/Bai011/11.cpp
+++ UIT_23520335_Function/Bai011/11.cpp
@@ -2,12 +2,12 @@
 #include<cmath>
 using namespace std;
 
-float do_dai_canh(int x1, int y1, int x2, int y2);
+float do_dai_canh(float x1, float y1, float x2, float y2);
 float dien_tich(float a, float b, float c);
 
 int main()
 {
-	int x1, y1, x2, y2, x3, y3;
+	float x1, y1, x2, y2, x3, y3;
 	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
 	float a = do_dai_canh(x1, y1, x2, y2);
 	float b = do_dai_canh(x2, y2, x3, y3);
@@ -16,7 +16,7 @@ int main()
 	return 0;
 }
 
-float do_dai_canh(int x1, int y1, int x2, int y2)
+float do_dai_canh(float x1, float y1, float x2, float y2)
 {
 	return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
