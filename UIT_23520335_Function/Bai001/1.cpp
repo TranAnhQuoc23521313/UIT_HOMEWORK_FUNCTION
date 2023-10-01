5f37f7e32bfaca3b461e200015aa6a2299398b97
TranAnhQuoc23521313 <146335703+TranAnhQuoc23521313@users.noreply.github.com>
2023-10-01 14:13:42 +0700

    Update 1.cpp

--- UIT_23520335_Function/Bai001/1.cpp
+++ UIT_23520335_Function/Bai001/1.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-float khoang_cach(int x1,int y1,int x2,int y2);
+float khoang_cach(float,float,float,float);
 
 int main()
 {
-	int x1, y1, x2, y2;
+	float x1, y1, x2, y2;
 	cin >> x1 >> y1 >> x2 >> y2;
 	cout << khoang_cach(x1, y1, x2, y2);
+	return 0;
 }
 
-float khoang_cach(int x1, int y1, int x2, int y2)
+float khoang_cach(float x1, float y1, float x2, float y2)
 {
 	float d = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 	return d;
