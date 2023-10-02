572f25501e28e9d104e106b2e87716b26447a5a8
QuangDung2511 <146616603+QuangDung2511@users.noreply.github.com>
2023-10-02 20:51:29 +0700

    Update 48.cpp

--- UIT_23520335_Function/Bai048/48.cpp
+++ UIT_23520335_Function/Bai048/48.cpp
@@ -2,17 +2,19 @@
 #include<cmath>
 using namespace std;
 
-float tich(int x,int n);
+float tich(float x,int n);
 
 int main()
 {
-	int x, n;
-	cin >> x >> n;
+	float x;
+	cin >> x;
+	int n;
+	cin >> n;
 	cout << tich(x,n);
 	return 0;
 }
 
-float tich(int x, int n)
+float tich(float x, int n)
 {
 	float t = x;
 	int i = 1;
