8c93e47c6053acd56a487d97d9b33ad256844879
QuangDung2511 <146616603+QuangDung2511@users.noreply.github.com>
2023-10-02 20:27:20 +0700

    Update 11.cpp

--- UIT_23520335_Function/Bai011/11.cpp
+++ UIT_23520335_Function/Bai011/11.cpp
@@ -2,8 +2,8 @@
 #include<cmath>
 using namespace std;
 
-float do_dai_canh(float x1, float y1, float x2, float y2);
-float dien_tich(float a, float b, float c);
+float do_dai_canh(float , float , float , float );
+float dien_tich(float , float , float );
 
 int main()
 {
