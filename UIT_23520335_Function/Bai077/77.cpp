77c1d18fd2e4175c31c7d2e1b646310fd4c093d9
TranAnhQuoc23521313 <146335703+TranAnhQuoc23521313@users.noreply.github.com>
2023-10-02 18:31:15 +0700

    Update 77.cpp !!!

--- UIT_23520335_Function/Bai077/77.cpp
+++ UIT_23520335_Function/Bai077/77.cpp
@@ -2,17 +2,18 @@
 #include<cmath>
 using namespace std;
 
-float tong(int k, int n);
+float tong(float k, int n);
 
 int main()
 {
-	int k, n;
+	int n;
+	float k;
 	cin >> k >> n;
 	cout << tong(k, n);
 	return 0;
 }
 
-float tong(int k, int n)
+float tong(float k, int n)
 {
 	float s = 0;
 	int i = 1;
