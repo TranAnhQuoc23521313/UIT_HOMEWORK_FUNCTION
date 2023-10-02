a8cbaf63049fb04d7223a1d76ddf608323019647
QuangDung2511 <146616603+QuangDung2511@users.noreply.github.com>
2023-10-02 20:28:39 +0700

    Update 12.cpp

--- UIT_23520335_Function/Bai012/12.cpp
+++ UIT_23520335_Function/Bai012/12.cpp
@@ -2,7 +2,7 @@
 #include<cmath>
 using namespace std;
 
-int phep_nhan(float x);
+float phep_nhan(float x);
 
 int main()
 {
@@ -12,10 +12,10 @@ int main()
 	return 0;
 }
 
-int phep_nhan(float x)
+float phep_nhan(float x)
 {
-	int x2 = x * x;
-	int x4 = x2 * x2;
-	int x6 = x4 * x2;
+	float x2 = x * x;
+	float x4 = x2 * x2;
+	float x6 = x4 * x2;
 	return x6;
 }
